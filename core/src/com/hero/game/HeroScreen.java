5345f8f0bc6f79847725c8e5168c5eabae772e7e
Kerry O'Neill <koneill7@binghamton.edu>
2021-05-20 19:30:35 -0400

    Started up new project to fix gradle installation and version issues, going off of original code

---
+++ core/src/com/hero/game/HeroScreen.java
@@ -0,0 +1,48 @@
+package com.hero.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class HeroScreen implements Screen {
+    OrthographicCamera orthoCamera;
+    public static float SCREEN_WIDTH = 1280;
+    public static float SCREEN_HEIGHT = 720;
+    public static float CAMERA_X  = SCREEN_WIDTH/2;
+    public static float CAMERA_Y = SCREEN_HEIGHT/2;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
+}
