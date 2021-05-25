81829bb2bfdfa737ff1f766f96e8badf8ae757b3
Kerry O'Neill <koneill7@binghamton.edu>
2021-05-25 00:47:41 -0400

    score

--- core/src/com/hero/game/MainStage.java
+++ core/src/com/hero/game/MainStage.java
@@ -3,6 +3,7 @@ package com.hero.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -44,15 +45,23 @@ public class MainStage extends Stage {
     Enemy enemy;
     private Listener listener = new Listener();
 
+    FreeTypeFont generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/timesBold.ttf"));
+    FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
     private Boolean moveRight = false;
     private Boolean moveLeft = false;
+    private Boolean jumping = false;
 
     private Boolean enemyLeft = true;
     private Boolean enemyRight = false;
     private Boolean attack = false;
-    private Texture buttonTextL, buttonTextR,attackText;
-    private Drawable buttonDrawL, buttonDrawR, attackDraw;
-    private ImageButton buttonL, buttonR, attackButton;
+    private Texture buttonTextL, buttonTextR,attackText, jumpText;
+    private Drawable buttonDrawL, buttonDrawR, attackDraw, jumpDraw;
+    private ImageButton buttonL, buttonR, attackButton, jumpButton;
+
+    private int score = 0;
+    private String newScore = "Score: 0";
+    BitmapFont bitmapFont = new BitmapFont();
 
     public MainStage(){
         super(new ExtendViewport(20F, 10F, new OrthographicCamera(20f, 10f)));
@@ -101,37 +110,70 @@ public class MainStage extends Stage {
         else if(moveRight){player.right();}
         else{player.stop();}
 
-        if(enemyLeft){enemy.left();}
-        else if(enemyRight){enemy.right();}
-        else{enemy.stop();}
+        if(enemy != null){
+            if(enemyLeft){enemy.left();}
+            else if(enemyRight){enemy.right();}
+            else{enemy.stop();}
+
+            if(enemy.getHealth() < 1){
+                score += 100;
+                newScore = "Score: " + score;
+
+                enemy.remove();
+                world.destroyBody(enemy.body);
+                enemy = new Enemy();
+                enemy.makeEnemy(world);
+                this.addActor(enemy);
+            }
+        }
+
+        if(listener.getContactMade() && !attack){
+            player.injured();
+
+        }
         if(listener.getContactMade() && attack){
-            System.out.println("attack succeeded");
+            enemy.injured(15);
+        }
+
+        if(listener.getGroundContact()){
+            player.land();
         }
     }
     @Override public void draw(){
         super.draw();
         render.render(world, getViewport().getCamera().combined);
+        getBatch().begin();
+        bitmapFont.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+        bitmapFont.draw(getBatch(), newScore, 8, 10);
+        bitmapFont.getData().setScale(3.0F,0.05F);
+        getBatch().end();
     }
 
     private void buttonGen(){
         buttonTextL = new Texture("leftbutton.png");
         buttonTextR = new Texture("rightbutton.png");
         attackText = new Texture("attackbutton.png");
+        jumpText = new Texture("jumpbutton.png");
         buttonDrawL = new TextureRegionDrawable(new TextureRegion(buttonTextL));
         buttonDrawR = new TextureRegionDrawable(new TextureRegion(buttonTextR));
         attackDraw = new TextureRegionDrawable(new TextureRegion(attackText));
+        jumpDraw = new TextureRegionDrawable(new TextureRegion(jumpText));
         buttonL = new ImageButton(buttonDrawL);
         buttonR = new ImageButton(buttonDrawR);
         attackButton = new ImageButton(attackDraw);
+        jumpButton = new ImageButton(jumpDraw);
         buttonL.setSize(0.5F, 0.5F);
         buttonR.setSize(0.5F, 0.5F);
         attackButton.setSize(0.9F, 0.5F);
+        jumpButton.setSize(0.5F, 0.5F);
         buttonL.setPosition(0.5F, 0.0F);
         buttonR.setPosition(1.3F, 0.0F);
         attackButton.setPosition(2.2F, 0.0F);
+        jumpButton.setPosition(3.1F, 0.0F);
         this.addActor(buttonL);
         this.addActor(buttonR);
         this.addActor(attackButton);
+        this.addActor(jumpButton);
         buttonL.addListener(new ActorGestureListener(){
             public void touchDown(InputEvent event, float x, float y, int pointer, int button){ moveLeft = true;}
             public void touchUp(InputEvent event, float x, float y, int pointer, int button){ moveLeft = false; }
@@ -143,6 +185,9 @@ public class MainStage extends Stage {
         attackButton.addListener(new ActorGestureListener(){
             public void touchDown(InputEvent event, float x, float y, int pointer, int button){ attack = true; }
             public void touchUp(InputEvent event, float x, float y, int pointer, int button){ attack = false; }
+        });
+        jumpButton.addListener(new ActorGestureListener(){
+            public void touchDown(InputEvent event, float x, float y, int pointer, int button){player.jump();}
         });
 
     }
